83a824089572a035e30ad31c26e841069a1fc6d8
Spasina Spasova <snikolowa2000@gmail.com>
2022-02-03 11:29:12 +0200

    DOM Introduction- Lab and Exercises

---
+++ DOM Introduction- Lab and Exercises/04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,10 @@
+function colorize() {
+    let allEvenRow=document.querySelectorAll('table tr')
+    
+    for(let i=0;i<allEvenRow.length;i++){
+        if(i%2!==0){
+            allEvenRow[i].style.background='Teal';
+        }
+    }
+    
+}
